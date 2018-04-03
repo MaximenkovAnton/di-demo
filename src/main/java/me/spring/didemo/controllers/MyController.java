e538cf61c8fab10eb656a86ea2696b45bbe8bb4f
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 16:28:23 +0300

    Added qualifier for MyController to choose service

--- src/main/java/me/spring/didemo/controllers/MyController.java
+++ src/main/java/me/spring/didemo/controllers/MyController.java
@@ -1,10 +1,19 @@
 package me.spring.didemo.controllers;
 
+import me.spring.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public void hello(){
+    private GreetingService greetingServiceImpl;
+
+    public MyController(@Qualifier("greetingServiceImpl") GreetingService greetingServiceImpl) {
+        this.greetingServiceImpl = greetingServiceImpl;
+    }
+
+    public String sayHello(){
         System.out.println("Hello");
+        return greetingServiceImpl.SayHello();
     }
 }
