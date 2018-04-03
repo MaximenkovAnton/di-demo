1d1efb098889973b52ead211c34b146c7395cd93
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 15:15:38 +0300

    Extracted hello field from greeting service implementation

--- src/main/java/me/spring/didemo/services/GreetingServiceImpl.java
+++ src/main/java/me/spring/didemo/services/GreetingServiceImpl.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
+
+    private static final String hello = "Hello!!!";
+
     @Override
     public String SayHello() {
-        return "Hello!!!";
+        return hello;
     }
 }
